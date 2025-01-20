537f28b6f768323b41239434dced35a2b60c39b0
swaaaney <akku.yermankyzy@bk.ru>
2025-01-20 12:00:49 +0500

    new file lab2

---
+++ lab2/3_lists.py
@@ -0,0 +1,19 @@
+#first example
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+"""
+second example
+"""
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+'''
+third example
+'''
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+
+#fourth example
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
