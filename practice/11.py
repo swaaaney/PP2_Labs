a1bc03c415bd3cad4ea1122f57e024f27c9dd6ec
swaaaney <akku.yermankyzy@bk.ru>
2025-03-03 11:56:10 +0500

    practice folder

--- practice/11.py
+++ practice/11.py
@@ -73,3 +73,18 @@ def create_file(dir, filename):
 
 my_fold = "backup"
 create_file(my_fold, "data.txt")
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+print(even_numbers)  # [2, 4, 6, 8, 10]
+
+def first_uppercase(text):
+    """Возвращает первую заглавную букву из строки"""
+    for char in text:
+        if char.isupper():
+            return char
+    return None  # Если заглавных букв нет
+
+print(first_uppercase("hello World"))  # ➝ 'W'
+print(first_uppercase("python"))       # ➝ None
+print(first_uppercase("I love You"))   # ➝ 'I'
